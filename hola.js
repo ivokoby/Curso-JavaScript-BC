c179a0fc79d0cfbec0fd43c6b406d70f3b81d2d8
ivokoby <ivokoby@gmail.com>
2022-07-06 17:50:05 -0300

    Primer commit

---
+++ hola.js
@@ -0,0 +1,10 @@
+console.log ("hola");
+
+var arr= [];
+
+for (var i=0; i<arr.length ; i++) {
+    if (arr[i]> i) {
+        return true;
+    }
+    return false;
+}
---
+++ keaa.js
@@ -0,0 +1,5 @@
+i= 2
+
+while (i===1){
+    return "Asa"
+}
