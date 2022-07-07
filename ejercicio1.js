07b84a46a2fe5d91d4649d8dafc54d810bd3460c
ivokoby <ivokoby@gmail.com>
2022-07-07 12:36:46 -0300

    Ejercicio1

---
+++ ejercicio1.js
@@ -0,0 +1,11 @@
+var lista = {
+    nombre : "Ivan",
+    edad : 20,
+    desarrollador: true,
+    date: "22/05/02",
+    libroFav : {
+        titulo: "Cosmos",
+        autor: "Carl Sagan",
+        fecha: 1978,
+    }
+}
