a09b026c427009e91ce036249c3ad02c15624c01
ivokoby <ivokoby@gmail.com>
2022-07-08 01:25:19 -0300

    ejercicio3

---
+++ ejercicio3.js
@@ -0,0 +1,23 @@
+
+var nombre = "Ivan";
+
+var apellido = "Koby";
+
+var estudiante = [];
+
+estudiante = nombre.concat(" ",apellido);
+
+estudianteMayus = estudiante.toUpperCase;
+
+estudianteMinus = estudiante.toLowerCase;
+
+var estudianteLetras = estudiante.length;
+
+var estudiantePrimeraLetra = estudiante[0];
+
+var estudianteUltimaLetra = estudiante[9];
+
+var estudianteSinEspacio = estudiante.trim;
+
+var estudianteNombre = estudiante.indexOf("Ivan");
+
