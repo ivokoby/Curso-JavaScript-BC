7a27be117c5611c6d41b02e43f3bc395fce817a8
ivokoby <ivokoby@gmail.com>
2022-07-08 11:34:02 -0300

    ejercicio4

---
+++ ejercicio4.js
@@ -0,0 +1,13 @@
+
+let alturaCm = 179;
+
+let alturaMtr = 1.79;
+
+let pesoKg = 80.5;
+
+let alturaRedondeada = Math.floor(alturaMtr);
+
+let pesoRedondeado = Math.ceil(pesoKg);
+
+number = 1
+let MaxValue = number.MAX_VALUE +1 === number.MAX_VALUE;
