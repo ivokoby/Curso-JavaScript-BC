f5d80d96fe73131d17a4ba35ff0d7ca58aeaabd7
ivokoby <ivokoby@gmail.com>
2022-07-10 21:53:08 -0300

    ejercicio8

---
+++ ejercicio8.js
@@ -0,0 +1,12 @@
+
+const fecha = new Date()
+
+const nacimiento = new Date(2002, 4, 22);
+
+var comparacion = (fecha > nacimiento);
+
+var dia = nacimiento.getDate()
+
+var mes = nacimiento.getMonth() + 1
+
+var año = nacimiento.getFullYear();
